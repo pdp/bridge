06a0df85a990be31f8743a5510f711a9d6d75f15
peterdp <peter.de.permentier@gmail.com>
2017-08-05 19:03:44 +0200

    rearranged project, added some components

---
+++ src/app/components/content-summary/content-summary.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-content-summary',
+  templateUrl: './content-summary.component.html',
+  styleUrls: ['./content-summary.component.scss']
+})
+export class ContentSummaryComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
