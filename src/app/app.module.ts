59b290c2492e006f52fb1ae13b5de017d3d4379a
peterdp <peter.de.permentier@gmail.com>
2017-08-05 17:54:40 +0200

    rearranged project, added some components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,17 @@ import { HttpModule } from '@angular/http';
 import { AppComponent } from './components/app/app.component';
 import {HeaderComponent} from './components/header/header.component';
 import {BannerComponent} from './components/banner/banner.component';
+import {NavigationComponent} from './components/navigation/navigation.component';
+import {ContentSummaryComponent} from './components/content-summary/content-summary.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    BannerComponent
+    BannerComponent,
+    NavigationComponent,
+    ContentSummaryComponent
   ],
   imports: [
     BrowserModule,
