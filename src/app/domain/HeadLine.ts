d81c5f47d8b2ba03c8f46a41ee35a85f11d3f01d
peterdp <peter.de.permentier@gmail.com>
2017-08-07 22:26:08 +0200

    remove testclasses

--- src/app/domain/HeadLine.ts
+++ src/app/domain/HeadLine.ts
@@ -7,8 +7,8 @@ export class Headline {
 }
 
 export const HEADLINES: Headline [] = [
-    {title: 'nieuws', content: ['Dit is de inhoud op de eerste regel.', 'Dit is de inhoud van de tweede paragraaf'], imageName: 'card_club_black.png'},
-    {title: 'activiteiten', content: ['Dit is de inhoud.'], imageName: 'card_heart_red.png'},
-    {title: 'blog', content: ['Dit is de inhoud.'], imageName: 'card_spade_black.png'},
-    {title: 'ledenbeheer', content: ['Dit is de inhoud.'], imageName: 'card_diamond_red.png'},
+    {title: 'nieuws', content: ['Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin bibendum ornare ante, in scelerisque ipsum ullamcorper in.', 'Nullam vel bibendum ligula. Donec hendrerit varius vestibulum. Proin imperdiet ante ac metus ultrices, ac faucibus ex congue. Suspendisse faucibus tempor lectus, sit amet vulputate velit malesuada commodo. Duis placerat non orci non fermentum. In tincidunt quam et ante iaculis convallis.'], imageName: 'card_club_black.png'},
+    {title: 'activiteiten', content: ['Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin bibendum ornare ante, in scelerisque ipsum ullamcorper in.', 'Nullam vel bibendum ligula. Donec hendrerit varius vestibulum. Proin imperdiet ante ac metus ultrices, ac faucibus ex congue. Suspendisse faucibus tempor lectus. Duis placerat non orci non fermentum. In tincidunt quam et ante iaculis convallis.'], imageName: 'card_heart_red.png'},
+    {title: 'blog', content: ['Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin bibendum ornare ante, in scelerisque ipsum ullamcorper in.', 'Nullam vel bibendum ligula. Donec hendrerit varius vestibulum. Proin imperdiet ante ac metus ultrices, ac faucibus ex congue. Suspendisse faucibus tempor lectus, sit amet vulputate velit malentum. In tincidunt quam et ante iaculis convallis.'], imageName: 'card_spade_black.png'},
+    {title: 'ledenbeheer', content: ['Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin bibendum ornare ante, in scelerisque ipsum ullamcorper in.', 'Nullam vel bibendum ligula. Donec hendrerit varius vestibulum. Proin imperdiet ante ac metus ultrices, ac faucibus ex congue. Suspendisse faucibus tempor lectus, sit amet vulputate velit malesuada commodo. Duis placerat non orci non fermentum. In tincidunt quam et ante iaculis convallis.'], imageName: 'card_diamond_red.png'},
   ];
